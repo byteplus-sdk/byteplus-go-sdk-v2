34c519e8305458d606bf750f59007ce72a0e5b95
xiujie.1 <xiujie.1@bytedance.com>
2024-12-11 16:35:24 +0800

    update

--- byteplus/byteplusutil/url.go
+++ byteplus/byteplusutil/url.go
@@ -26,12 +26,7 @@ type ServiceInfo struct {
 	Region  string
 }
 
-const (
-	endpoint = "open.byteplusapi.com"
-	//internalUrl = "open.byteplusapi.com"
-	//http  = "http"
-	//https = "https"
-)
+var endpoint = "open.ap-southeast-1.byteplusapi.com"
 
 func (c *Endpoint) GetEndpoint() string {
 	if c.CustomerEndpoint != "" {
@@ -39,4 +34,69 @@ func (c *Endpoint) GetEndpoint() string {
 	} else {
 		return endpoint
 	}
+}
+
+type RegionEndpointMap map[string]string
+
+type ServiceEndpointInfo struct {
+	Service         string
+	IsGlobal        bool
+	GlobalEndpoint  string
+	DefaultEndpoint string
+	RegionEndpointMap
+}
+
+var defaultEndpoint = map[string]*ServiceEndpointInfo{
+	"billing": {
+		Service:           "billing",
+		IsGlobal:          true,
+		GlobalEndpoint:    "",
+		DefaultEndpoint:   "open.byteplusapi.com",
+		RegionEndpointMap: nil,
+	},
+}
+
+// GetDefaultEndpointByServiceInfo retrieves the default endpoint for a given service and region.
+//
+// This function takes in the service name and region code, and returns a pointer to the default
+// endpoint string. It checks if the service has a global endpoint or a region-specific endpoint.
+// If neither is found, it returns a pointer to the default endpoint.
+//
+// Parameters:
+// - service: The name of the service for which to retrieve the endpoint.
+// - regionCode: The region code to look up the region-specific endpoint.
+//
+// Returns:
+// - *string: A pointer to the endpoint string. It could be a global endpoint, a region-specific
+// endpoint, or a default endpoint if the specified service or region does not have a defined endpoint.
+//
+// Example:
+//
+//	endpoint := GetDefaultEndpointByServiceInfo("exampleService", "cn-beijing")
+//
+// Note: Ensure the `defaultEndpoint` map is properly populated with service and region endpoint
+// information before calling this function.
+func GetDefaultEndpointByServiceInfo(service string, regionCode string) *string {
+	defaultEndpointInfo, sExist := defaultEndpoint[service]
+	if !sExist {
+		return &endpoint
+	}
+
+	isGlobal := defaultEndpointInfo.IsGlobal
+	if isGlobal {
+		if len(defaultEndpointInfo.GlobalEndpoint) > 0 {
+			return &defaultEndpointInfo.GlobalEndpoint
+		}
+	} else {
+		regionEndpointMp := defaultEndpointInfo.RegionEndpointMap
+		regionEndpointStr, rExist := regionEndpointMp[regionCode]
+		if rExist {
+			return &regionEndpointStr
+		}
+	}
+
+	if len(defaultEndpointInfo.DefaultEndpoint) > 0 {
+		return &defaultEndpointInfo.DefaultEndpoint
+	}
+	return &endpoint
 }
