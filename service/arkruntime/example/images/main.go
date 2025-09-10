d95d5262906569679f5ee6813562bed314d22130
yiguang <yiguang@bytedance.com>
2025-09-10 11:37:45 +0800

    feat: add seedream stream sdk

--- service/arkruntime/example/images/main.go
+++ service/arkruntime/example/images/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/byteplus-sdk/byteplus-go-sdk-v2/byteplus"
 	"github.com/byteplus-sdk/byteplus-go-sdk-v2/service/arkruntime"
@@ -20,7 +22,7 @@ import (
 func main() {
 	client := arkruntime.NewClientWithApiKey(os.Getenv("ARK_API_KEY"))
 	ctx := context.Background()
-	modelEp := "YOUR_ENDPOINT_ID"
+	modelEp := "${YOUR_ENDPOINT_ID}"
 
 	fmt.Println("----- [Seedream] generate images (response format: url) -----")
 	generateReq := model.GenerateImagesRequest{
@@ -29,8 +31,7 @@ func main() {
 		ResponseFormat: byteplus.String(model.GenerateImagesResponseFormatURL),
 		Seed:           byteplus.Int64(1234567890),
 		Watermark:      byteplus.Bool(true),
-		Size:           byteplus.String("512x512"),
-		GuidanceScale:  byteplus.Float64(2.5),
+		Size:           byteplus.String("1024x1024"),
 	}
 
 	imagesResponse, err := client.GenerateImages(ctx, generateReq)
@@ -56,8 +57,7 @@ func main() {
 		ResponseFormat: byteplus.String(model.GenerateImagesResponseFormatBase64),
 		Seed:           byteplus.Int64(1234567890),
 		Watermark:      byteplus.Bool(true),
-		Size:           byteplus.String("512x512"),
-		GuidanceScale:  byteplus.Float64(2.5),
+		Size:           byteplus.String("1024x1024"),
 	}
 
 	imagesResponse, err = client.GenerateImages(ctx, generateReq)
@@ -78,13 +78,12 @@ func main() {
 
 	generateReq = model.GenerateImagesRequest{
 		Model:          modelEp, // Replace with your endpoint ID
-		Prompt:         "龙与地下城女骑士背景是起伏的平原，目光从镜头转向平原",
+		Prompt:         "Bird soaring above vast grasslands",
 		Image:          byteplus.String("YOUR_IMAGE_URL_HERE"), // Replace with your input image URL
 		ResponseFormat: byteplus.String(model.GenerateImagesResponseFormatURL),
 		Seed:           byteplus.Int64(1234567890),
 		Watermark:      byteplus.Bool(true),
-		Size:           byteplus.String(model.GenerateImagesSizeAdaptive),
-		GuidanceScale:  byteplus.Float64(2.5),
+		Size:           byteplus.String("1024x1024"),
 	}
 	imagesResponse, err = client.GenerateImages(ctx, generateReq)
 	if err != nil {
@@ -102,13 +101,12 @@ func main() {
 	fmt.Println("----- [Seededit] generate images (input format: base64) -----")
 	generateReq = model.GenerateImagesRequest{
 		Model:          modelEp, // Replace with your Seededit endpoint ID
-		Prompt:         "龙与地下城女骑士背景是起伏的平原，目光从镜头转向平原",
+		Prompt:         "Bird soaring above vast grasslands",
 		Image:          byteplus.String("YOUR_IMAGE_BASE64_HERE"), // Replace with your input image base64 data url
 		ResponseFormat: byteplus.String(model.GenerateImagesResponseFormatURL),
 		Seed:           byteplus.Int64(1234567890),
 		Watermark:      byteplus.Bool(true),
-		Size:           byteplus.String(model.GenerateImagesSizeAdaptive),
-		GuidanceScale:  byteplus.Float64(2.5),
+		Size:           byteplus.String("1024x1024"),
 	}
 	imagesResponse, err = client.GenerateImages(ctx, generateReq)
 	if err != nil {
@@ -123,4 +121,112 @@ func main() {
 	fmt.Printf("Image URL: %s\n", *imagesResponse.Data[0].Url)
 	fmt.Printf("Generated Images: %d\n", imagesResponse.Usage.GeneratedImages)
 	fmt.Printf("Created: %d\n", imagesResponse.Created)
+
+	fmt.Println("----- [Seedream] streaming generate images (response format: url) -----")
+	var sequentialImageGeneration model.SequentialImageGeneration = "auto"
+	maxImages := 9
+	generateReq = model.GenerateImagesRequest{
+		Model:                     modelEp, // Replace with your Seedream endpoint ID
+		Prompt:                    "Bird soaring above vast grasslands",
+		ResponseFormat:            byteplus.String(model.GenerateImagesResponseFormatURL),
+		Seed:                      byteplus.Int64(1234567890),
+		Watermark:                 byteplus.Bool(true),
+		Size:                      byteplus.String("1024x1024"),
+		SequentialImageGeneration: &sequentialImageGeneration,
+		SequentialImageGenerationOptions: &model.SequentialImageGenerationOptions{
+			MaxImages: &maxImages,
+		},
+	}
+
+	stream, err := client.GenerateImagesStreaming(ctx, generateReq)
+	if err != nil {
+		fmt.Printf("call GenerateImagesStreaming error: %v\n", err)
+		return
+	}
+
+	defer stream.Close()
+
+	for {
+		recv, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Stream generate images error: %v\n", err)
+			break
+		}
+
+		if recv.Type == "image_generation.partial_failed" {
+			fmt.Printf("Stream generate images error: %v\n", recv.Error)
+			if strings.EqualFold(recv.Error.Code, "InternalServiceError") {
+				break
+			}
+		}
+
+		if recv.Type == "image_generation.partial_succeeded" {
+			if recv.Error == nil && recv.Url != nil {
+				fmt.Printf("recv.Size: %s, recv.Url: %s\n", recv.Size, *recv.Url)
+			}
+		}
+
+		if recv.Type == "image_generation.completed" {
+			if recv.Error == nil {
+				fmt.Printf("recv.Usage: %v\n", *recv.Usage)
+			}
+		}
+	}
+
+	fmt.Println("----- [Seededit] streaming generate images (response format: base64) -----")
+
+	generateReq = model.GenerateImagesRequest{
+		Model:                     modelEp, // Replace with your Seedream endpoint ID
+		Prompt:                    "Bird soaring above vast grasslands",
+		ResponseFormat:            byteplus.String(model.GenerateImagesResponseFormatBase64),
+		Seed:                      byteplus.Int64(1234567890),
+		Watermark:                 byteplus.Bool(true),
+		Size:                      byteplus.String("1024x1024"),
+		Image:                     []string{"YOUR_IMAGE_URL_HERE", "YOUR_IMAGE_URL_HERE"}, // Replace with your input image URL
+		SequentialImageGeneration: &sequentialImageGeneration,
+		SequentialImageGenerationOptions: &model.SequentialImageGenerationOptions{
+			MaxImages: &maxImages,
+		},
+	}
+
+	stream, err = client.GenerateImagesStreaming(ctx, generateReq)
+	if err != nil {
+		fmt.Printf("call GenerateImagesStreaming error: %v\n", err)
+		return
+	}
+
+	defer stream.Close()
+
+	for {
+		recv, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Stream generate images error: %v\n", err)
+			break
+		}
+
+		if recv.Type == "image_generation.partial_failed" {
+			fmt.Printf("Stream generate images error: %v\n", recv.Error)
+			if strings.EqualFold(recv.Error.Code, "InternalServiceError") {
+				break
+			}
+		}
+
+		if recv.Type == "image_generation.partial_succeeded" {
+			if recv.Error == nil && recv.B64Json != nil {
+				fmt.Printf("recv.Size: %s, recv.B64Json: %s\n", recv.Size, *recv.B64Json)
+			}
+		}
+
+		if recv.Type == "image_generation.completed" {
+			if recv.Error == nil {
+				fmt.Printf("recv.Usage: %v\n", *recv.Usage)
+			}
+		}
+	}
 }
