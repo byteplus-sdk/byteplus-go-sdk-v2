c909cdf9ee4ec374ba5b6d6b14cd90b69b219201
Wang Zesong <zesong.wang1.bytedance.com>
2025-09-22 18:25:03 +0800

    fix: remove unused

--- service/arkruntime/utils/stream_reader.go
+++ service/arkruntime/utils/stream_reader.go
@@ -31,6 +31,10 @@ type BotChatCompletionStreamReader struct {
 	ChatCompletionStreamReader
 }
 
+type ImageGenerationStreamReader struct {
+	ChatCompletionStreamReader
+}
+
 func (stream *ChatCompletionStreamReader) Recv() (response model.ChatCompletionStreamResponse, err error) {
 	if stream.IsFinished {
 		err = io.EOF
@@ -153,7 +157,6 @@ func (stream *ImageGenerationStreamReader) processLines() (model.ImagesStreamRes
 		unmarshalErr := stream.Unmarshaler.Unmarshal(trimedLine, &response)
 		if unmarshalErr != nil {
 			return model.ImagesStreamResponse{}, unmarshalErr
-
 		}
 
 		return response, nil
@@ -189,4 +192,8 @@ func (stream *BotChatCompletionStreamReader) Close() error {
 	// fmt.Printf("%#v\n", stream.Response)
 	// fmt.Printf("%#v\n", stream.Response.Body)
 	return stream.ChatCompletionStreamReader.Response.Body.Close()
+}
+
+func (stream *ImageGenerationStreamReader) Close() error {
+	return stream.Response.Body.Close()
 }
