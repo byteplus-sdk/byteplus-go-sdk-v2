989b6ce40e743a8b03a218ce3afbcd4f6baba235
yuguo.dtpe <yuguo.dtpe@bytedance.com>
2026-02-05 14:37:32 +0800

    chore: adjust sso cache dir

--- byteplus/credentials/clicreds/cli_provider.go
+++ byteplus/credentials/clicreds/cli_provider.go
@@ -92,10 +92,10 @@ func NewCliCredentials(configPath, profile string) *credentials.Credentials {
 		home := shareddefaults.UserHomeDir()
 		if home != "" {
 			configPath = filepath.Join(home, ".volcengine", "config.json")
-			cacheDir = filepath.Join(home, ".volcengine", "cache")
+			cacheDir = filepath.Join(home, ".volcengine", "sso", "cache")
 		}
 	} else {
-		cacheDir = filepath.Join(filepath.Dir(configPath), "cache")
+		cacheDir = filepath.Join(filepath.Dir(configPath), "sso", "cache")
 	}
 	return credentials.NewExpireAbleCredentials(&CliProvider{
 		configPath: configPath,
