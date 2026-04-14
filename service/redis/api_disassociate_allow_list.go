1ee03c58764d24345127ee4be302e21f651c411c
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-04-14 20:24:56 +0800

    feat: auto generate for redis-Golang-2020-12-07-online-2547-2026_04_14_20_24_49

--- service/redis/api_disassociate_allow_list.go
+++ service/redis/api_disassociate_allow_list.go
@@ -146,11 +146,11 @@ func (c *REDIS) DisassociateAllowListWithContext(ctx byteplus.Context, input *Di
 type DisassociateAllowListInput struct {
 	_ struct{} `type:"structure" json:",omitempty"`
 
-	AllowListIds []*string `type:"list" json:",omitempty"`
+	AllowListIds []*string `type:"list"`
 
 	ClientToken *string `type:"string" json:",omitempty"`
 
-	InstanceIds []*string `type:"list" json:",omitempty"`
+	InstanceIds []*string `type:"list"`
 }
 
 // String returns the string representation
